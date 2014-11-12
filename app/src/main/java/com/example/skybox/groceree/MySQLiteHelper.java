cb94e6d0a2111c12a29b7bcbbe280c11b23c81df
skybox <skybox@trioptimum.com>
2014-11-11 22:15:19 -0600

    Starting work on item database.

---
+++ app/src/main/java/com/example/skybox/groceree/MySQLiteHelper.java
@@ -0,0 +1,42 @@
+package com.example.skybox.groceree;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by skybox on 11/11/14.
+ */
+public class MySQLiteHelper extends SQLiteOpenHelper {
+    public static final String TABLE_ITEM = "item";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_ITEM = "item";
+    public static final String COLUMN_DELETED = "deleted";
+
+
+    private static final String DATABASE_NAME = "item.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // DB creation statement
+    private static final String DATABASE_CREATE = "create table " + TABLE_ITEM
+            + "(" + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_ITEM + " text not null, "
+            + COLUMN_DELETED + " integer not null";
+
+    public MySQLiteHelper( Context context ) {
+        super( context, DATABASE_NAME, null, DATABASE_VERSION );
+    }
+
+    public void onCreate( SQLiteDatabase database ) {
+        database.execSQL( DATABASE_CREATE );
+    }
+
+    public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion ) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                + newVersion );
+        db.execSQL( "DROP TABLE IF EXISTS " + TABLE_ITEM );
+        onCreate( db );
+    }
+}
